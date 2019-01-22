a59130be4487dfdea02f0b77eeb7f996bc10a709
jisu <jisu@HONG-SIN.local>
2019-01-22 22:34:02 +0900

    Plan plist 구현 중

--- DonggukGraduation/DonggukGraduation/Model/Lecture.swift
+++ DonggukGraduation/DonggukGraduation/Model/Lecture.swift
@@ -5,10 +5,26 @@ class Lecture {
     var name: String
     var category: String
     var credit: Int
-    
+
     init(name: String, category: String, credit: Int){
         self.name = name
         self.category = category
         self.credit = credit
+    }
+    
+//    init?(dict: [String: Any]){
+//        guard let nameValue = dict["name"] as? String else { return nil }
+//        guard let categoryValue = dict["name"] as? String else { return nil }
+//        guard let nameValue = dict["name"] as? String else { return nil }
+//    }
+//
+//    func toDict() -> [String: Any] {
+//        return ["name": name, "capacity": capacity]
+//    }
+}
+
+extension Lecture: CustomStringConvertible {
+    var description: String {
+        return "<Lecture: \(name), \(category), \(credit)>"
     }
 }
